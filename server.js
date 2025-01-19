37f2722b63616933c82d367fa275076af74acb44
Max Miller <67687590+maxtmiller@users.noreply.github.com>
2025-01-19 05:28:37 -0500

    Update

--- server.js
+++ server.js
@@ -23,8 +23,6 @@ const client = new MongoClient(process.env.MONGODB_URI, {
     }
 });
 
-// const upload = multer({ dest: 'uploads/' });
-
 async function connectToDatabase() {
     try {
         if (!client.isConnected) {
@@ -37,82 +35,6 @@ async function connectToDatabase() {
     }
 }
 
-async function createCollection() {
-    try {
-        // Connect to MongoDB server
-        await client.connect();
-        console.log("Connected to MongoDB!");
-
-        // Specify the database
-        const db = client.db('webapp'); // Replace with your actual database name
-
-        // Check if the 'models' collection exists
-        const collections = await db.listCollections().toArray();
-        const collectionNames = collections.map(col => col.name);
-
-        // If 'models' collection doesn't exist, create it
-        if (!collectionNames.includes('models')) {
-            await db.createCollection('models');
-            console.log("Collection 'models' created.");
-        }
-
-        // Proceed with file upload and insertion
-        const filePath = path.join(__dirname, 'trained_model.h5');
-        const bucket = new GridFSBucket(db, { bucketName: 'models' });
-
-        // Create a readable stream from the file
-        const fileStream = fs.createReadStream(filePath);
-
-        // Open a GridFS upload stream
-        const uploadStream = bucket.openUploadStream('trained_model.h5');
-
-        const { size } = fs.statSync(filePath); // size is in bytes
-
-        // Convert size to a more readable format (e.g., KB, MB)
-        const formatSize = (size) => {
-            const units = ['B', 'KB', 'MB', 'GB', 'TB'];
-            let index = 0;
-            let fileSize = size;
-
-            while (fileSize >= 1024 && index < units.length - 1) {
-                fileSize /= 1024;
-                index++;
-            }
-
-            return `${fileSize.toFixed(2)} ${units[index]}`;
-        };
-
-        const formattedSize = formatSize(size);
-
-        // Wait for the file to be uploaded
-        await new Promise((resolve, reject) => {
-            // Pipe the file to GridFS
-            fileStream.pipe(uploadStream);
-
-            // Handle upload completion and error
-            uploadStream.on('finish', resolve);
-            uploadStream.on('error', reject);
-        });
-
-        // After upload completes, insert metadata into the collection
-        const modelsCollection = db.collection('models');
-        const result = await modelsCollection.insertOne({
-            userId: 'development',
-            name: 'AI Model',
-            type: 'NLP',
-            size: formattedSize,
-            list: false,
-            description: '',
-            file: uploadStream.id,
-            lastUpdated: new Date(),
-        });
-
-        console.log("Model metadata inserted with file ID:", uploadStream.id);
-    } catch (err) {
-        console.error('Error in creating collection or uploading file:', err);
-    }
-}
-createCollection();
 
 function deleteFilesInFolder(folderPath) {
     fs.readdir(folderPath, (err, files) => {
@@ -139,11 +61,8 @@ function deleteFilesInFolder(folderPath) {
                         }
                     });
                 }
-                // Optionally handle subfolders
                 else if (stats.isDirectory()) {
                     console.log(`Skipping directory: ${filePath}`);
-                    // Uncomment below to recursively delete subfolders
-                    // deleteFilesInFolder(filePath);
                 }
             });
         });
@@ -172,7 +91,6 @@ app.get('/', (req, res) => {
         res.render('dashboard', { userName });
     } else {
         res.redirect('/login');
-        // res.sendFile(path.join(__dirname, './public/login.html'));
     }
 });
 
@@ -231,23 +149,6 @@ app.get('/model/:id', async (req, res) => {
         res.render('detail', { models, users });
     } catch (err) {
         res.status(500).json({ error: err.message });
-    }
-});
-
-
-app.get('/detail/:id', async (req, res) => {
-    try {
-        await client.connect();
-        const db = client.db('webapp');
-        const collection = db.collection('models');
-        // const bucket = new GridFSBucket(db, { bucketName: 'models' });
-
-        const modelId = req.params.id;
-        const fileDocument = await collection.findOne({ _id: new ObjectId(modelId) });
-        
-        render('detail')
-    } catch (err) {
-        res.status(500).send('Error: ' + err.message);
     }
 });
 
@@ -356,13 +257,12 @@ app.get('/cart', async (req, res) => {
     }
 });
 
-// Set up multer for file uploads
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');  // Save uploaded files in the 'uploads' folder
+        cb(null, 'uploads/'); 
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname));  // Add timestamp to file name
+        cb(null, Date.now() + path.extname(file.originalname)); 
     }
 });
 
@@ -373,13 +273,7 @@ app.post('/upload', upload.fields([{ name: 'modelFile', maxCount: 1 }]), async (
         const { modelName, modelDescription, modelType } = req.body;
         const file = req.files['modelFile'] ? req.files['modelFile'][0] : null;
         const path_to_file = file.filename;
-        
-        // console.log(req.body);
-        // console.log(req.oidc.user);
-        // console.log(path_to_file);
-        // console.log(modelName, modelDescription, modelType);
-        // res.status(200).send('Model uploaded successfully!');
-
+    
         const filePath = path.join(__dirname, 'uploads', path_to_file);
 
         await connectToDatabase();
@@ -444,23 +338,19 @@ app.get('/download/:id', async (req, res) => {
     const fileId = req.params.id;
 
     try {
-        // Log the incoming request and the fileId
         console.log(`Received download request for file ID: ${fileId}`);
 
-        // Validate the fileId
         if (!ObjectId.isValid(fileId)) {
             console.log('Invalid ObjectId:', fileId);
             return res.status(400).send('Invalid file ID.');
         }
 
-        // Connect to the database
         await connectToDatabase();
-        const db = client.db('webapp'); // Get the database
+        const db = client.db('webapp');
         const collection = db.collection('models');
 
         console.log('Database connection successful.');
 
-        // Retrieve the file document by ID
         const fileDocument = await collection.findOne({ _id: new ObjectId(fileId) });
 
         if (!fileDocument) {
@@ -468,33 +358,26 @@ app.get('/download/:id', async (req, res) => {
             return res.status(404).send('File not found.');
         }
 
-        // Retrieve the file from GridFS
         const bucket = new GridFSBucket(db, { bucketName: 'models' });
         const fileStream = bucket.openDownloadStream(new ObjectId(fileDocument.file));
 
-        // Set the headers for the response to prompt the file download
         res.setHeader('Content-Disposition', 'attachment; filename="downloaded-file.h5"');
         res.setHeader('Content-Type', 'application/octet-stream');
 
-        // Pipe the file stream to the response
         fileStream.pipe(res);
 
-        // Handle errors from the file stream
         fileStream.on('error', (err) => {
             console.error('Error retrieving file:', err);
             return res.status(500).send('Error downloading the file.');
         });
 
-        // Handle successful completion of the stream
         fileStream.on('finish', () => {
             console.log('File download stream finished.');
         });
 
     } catch (err) {
-        // Log the detailed error
         console.error('Error occurred during the download process:', err);
 
-        // Send a generic error response
         return res.status(500).send('Error retrieving the file.');
     }
 });
